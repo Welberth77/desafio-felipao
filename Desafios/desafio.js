8e3b2c2a7159e54729faeeafce76110a62efa8e5
Welberth <welbertholiveira250@gmail.com>
2024-05-14 08:40:23 -0300

    Entrega do desafio classificador nível héroi

---
+++ Desafios/desafio.js
@@ -0,0 +1,15 @@
+console.log("==== DESAFIO CLASSIFICADOR NÍVEL HÉROI ====")
+let nome = ("welberth")
+let experiencia = 10050
+
+if (experiencia >= 0 && experiencia <= 1000){nivel = "Ferro"}
+else if (experiencia > 1000 && experiencia <= 2000) {nivel = "Bronze"}
+else if (experiencia > 2000 && experiencia <= 5000) {nivel = "Prata"}
+else if (experiencia > 5000 && experiencia <= 7000) {nivel = "Ouro"}
+else if (experiencia > 7000 && experiencia <= 8000) {nivel = "Platina"}
+else if (experiencia > 8000 && experiencia <= 9000) {nivel = "Ascendente"}
+else if (experiencia > 9000 && experiencia <= 10000) {nivel = "Imortal"}
+else if (experiencia > 10000) {nivel = "Radiante"}
+else {nivel = "Não encontrado"}
+
+console.log("O héroi de nome " + nome + " está no nível de " + nivel)
